21d894ac55eb209a5c99d8df0a33b7a2e02a800a
Ian Lauderman <ialauder@broadcom.com>
2016-03-23 10:40:57 -0400

    SW7439-846: Set the MAI Input rate contrl mode to Locked only for AAC input and locked for everthing else.

--- magnum/portinginterface/ape/src/raaga/bape_mai_input.c
+++ magnum/portinginterface/ape/src/raaga/bape_mai_input.c
@@ -1,43 +1,39 @@
 /******************************************************************************
  * Broadcom Proprietary and Confidential. (c)2016 Broadcom. All rights reserved.
  *
- * This program is the proprietary software of Broadcom and/or its
- * licensors, and may only be used, duplicated, modified or distributed pursuant
- * to the terms and conditions of a separate, written license agreement executed
- * between you and Broadcom (an "Authorized License").  Except as set forth in
- * an Authorized License, Broadcom grants no license (express or implied), right
- * to use, or waiver of any kind with respect to the Software, and Broadcom
- * expressly reserves all rights in and to the Software and all intellectual
- * property rights therein.  IF YOU HAVE NO AUTHORIZED LICENSE, THEN YOU
+ * This program is the proprietary software of Broadcom and/or its licensors,
+ * and may only be used, duplicated, modified or distributed pursuant to the terms and
+ * conditions of a separate, written license agreement executed between you and Broadcom
+ * (an "Authorized License").  Except as set forth in an Authorized License, Broadcom grants
+ * no license (express or implied), right to use, or waiver of any kind with respect to the
+ * Software, and Broadcom expressly reserves all rights in and to the Software and all
+ * intellectual property rights therein.  IF YOU HAVE NO AUTHORIZED LICENSE, THEN YOU
  * HAVE NO RIGHT TO USE THIS SOFTWARE IN ANY WAY, AND SHOULD IMMEDIATELY
  * NOTIFY BROADCOM AND DISCONTINUE ALL USE OF THE SOFTWARE.
  *
  * Except as expressly set forth in the Authorized License,
  *
- * 1. This program, including its structure, sequence and organization,
- *    constitutes the valuable trade secrets of Broadcom, and you shall use all
- *    reasonable efforts to protect the confidentiality thereof, and to use
- *    this information only in connection with your use of Broadcom integrated
- *    circuit products.
+ * 1.     This program, including its structure, sequence and organization, constitutes the valuable trade
+ * secrets of Broadcom, and you shall use all reasonable efforts to protect the confidentiality thereof,
+ * and to use this information only in connection with your use of Broadcom integrated circuit products.
  *
- * 2. TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"
- *    AND WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR
- *    WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT
- *    TO THE SOFTWARE.  BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED
- *    WARRANTIES OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A
- *    PARTICULAR PURPOSE, LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET
- *    ENJOYMENT, QUIET POSSESSION OR CORRESPONDENCE TO DESCRIPTION. YOU ASSUME
- *    THE ENTIRE RISK ARISING OUT OF USE OR PERFORMANCE OF THE SOFTWARE.
+ * 2.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"
+ * AND WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR
+ * WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT TO
+ * THE SOFTWARE.  BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES
+ * OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR PURPOSE,
+ * LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET POSSESSION
+ * OR CORRESPONDENCE TO DESCRIPTION. YOU ASSUME THE ENTIRE RISK ARISING OUT OF
+ * USE OR PERFORMANCE OF THE SOFTWARE.
  *
- * 3. TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS
- *    LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT,
- *    OR EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO
- *    YOUR USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN
- *    ADVISED OF THE POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS
- *    OF THE AMOUNT ACTUALLY PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER
- *    IS GREATER. THESE LIMITATIONS SHALL APPLY NOTWITHSTANDING ANY FAILURE OF
- *    ESSENTIAL PURPOSE OF ANY LIMITED REMEDY.
- *
+ * 3.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS
+ * LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT, OR
+ * EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO YOUR
+ * USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS OF THE AMOUNT
+ * ACTUALLY PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER IS GREATER. THESE
+ * LIMITATIONS SHALL APPLY NOTWITHSTANDING ANY FAILURE OF ESSENTIAL PURPOSE OF
+ * ANY LIMITED REMEDY.
  *****************************************************************************/
 
 #include "bape.h"
@@ -850,8 +846,6 @@ static BERR_Code BAPE_MaiInput_P_OpenHw(BAPE_MaiInputHandle handle)
         BAPE_Reg_P_Write(hApe, BCHP_AUD_FMM_IOP_MISC_ESR_STATUS_CLEAR, BCHP_MASK( AUD_FMM_IOP_MISC_ESR_STATUS_CLEAR, IOP_INTERRUPT_IN_HDMI_0));
         BAPE_Reg_P_Write(hApe, BCHP_AUD_FMM_IOP_MISC_ESR_MASK_CLEAR,   BCHP_MASK( AUD_FMM_IOP_MISC_ESR_MASK_CLEAR,   IOP_INTERRUPT_IN_HDMI_0));
 
-        BAPE_Reg_P_UpdateEnum_isr(hApe, BCHP_AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL,
-                                                           AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL, MODE, Locked);
 
     #endif /* if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_IOPIN */
     #if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_LEGACY
@@ -863,8 +857,6 @@ static BERR_Code BAPE_MaiInput_P_OpenHw(BAPE_MaiInputHandle handle)
                                                               BCHP_MASK( SPDIF_RCVR_ESR_MASK_CLEAR, SPDIFRX_COMP_CHANGE_MASK ) |
                                                               BCHP_MASK( SPDIF_RCVR_ESR_MASK_CLEAR, SPDIFRX_MAI_FMT_CHANGE_MASK ));
 
-        BAPE_Reg_P_UpdateEnum_isr(hApe, BCHP_HDMI_RCVR_CTRL_OUT_RATE_CTRL,
-                                                            SPDIF_RCVR_CTRL_OUT_RATE_CTRL, MODE, Locked);
     #endif /* if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_LEGACY */
 
     {
@@ -1390,6 +1382,40 @@ static void BAPE_MaiInput_P_SetReceiverOutputFormat_isr (BAPE_MaiInputHandle han
                 outFormatEna =  BAPE_P_BCHP_ENUM(BAPE_P_HDMI_RX_CONFIG_REGNAME, OUT_FORMAT_ENA, PES);
             }
         }
+
+        if (pFormatDetectionStatus->codec == BAVC_AudioCompressionStd_eAacAdts ||
+            pFormatDetectionStatus->codec == BAVC_AudioCompressionStd_eAacLoas ||
+            pFormatDetectionStatus->codec == BAVC_AudioCompressionStd_eAacPlusAdts ||
+            pFormatDetectionStatus->codec == BAVC_AudioCompressionStd_eAacPlusAdts ) {
+#if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_IOPIN
+            BAPE_Reg_P_UpdateEnum_isr(handle->deviceHandle, BCHP_AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL,
+                                      AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL, MODE, Locked);
+#endif
+#if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_LEGACY
+            BAPE_Reg_P_UpdateEnum_isr(handle->deviceHandle, BCHP_HDMI_RCVR_CTRL_OUT_RATE_CTRL,
+                                      SPDIF_RCVR_CTRL_OUT_RATE_CTRL, MODE, Locked);
+#endif
+        }
+        else {
+#if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_IOPIN
+            BAPE_Reg_P_UpdateEnum_isr(handle->deviceHandle, BCHP_AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL,
+                                      AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL, MODE, Keep_empty);
+#endif
+#if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_LEGACY
+            BAPE_Reg_P_UpdateEnum_isr(handle->deviceHandle, BCHP_HDMI_RCVR_CTRL_OUT_RATE_CTRL,
+                                      SPDIF_RCVR_CTRL_OUT_RATE_CTRL, MODE, Keep_empty);
+#endif
+        }
+    }
+    else {
+#if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_IOPIN
+            BAPE_Reg_P_UpdateEnum_isr(handle->deviceHandle, BCHP_AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL,
+                                      AUD_FMM_IOP_IN_HDMI_0_OUT_RATE_CTRL, MODE, Keep_empty);
+#endif
+#if defined BAPE_CHIP_MAI_INPUT_TYPE_IS_LEGACY
+            BAPE_Reg_P_UpdateEnum_isr(handle->deviceHandle, BCHP_HDMI_RCVR_CTRL_OUT_RATE_CTRL,
+                                      SPDIF_RCVR_CTRL_OUT_RATE_CTRL, MODE, Keep_empty);
+#endif
     }
 
     if ( outFormatEna != handle->outFormatEna )
