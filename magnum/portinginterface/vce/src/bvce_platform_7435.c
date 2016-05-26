00877674bbcb613d2ca9f5d6bfa2e1153b3a9d77
Nilesh Agarwalla <nilesh@broadcom.com>
2016-05-26 16:12:02 -0400

    SWSTB-1877: Ensure ARCs are asleep prior to releasing the clock (URSR 16.1)
    (cherry picked from commit 4b81eb46d90c352ede75998ed446d37e476f0548)

--- magnum/portinginterface/vce/src/bvce_platform_7435.c
+++ magnum/portinginterface/vce/src/bvce_platform_7435.c
@@ -1,22 +1,42 @@
 /***************************************************************************
- *     Copyright (c) 2003-2014, Broadcom Corporation
- *     All Rights Reserved
- *     Confidential Property of Broadcom Corporation
+ * Broadcom Proprietary and Confidential. (c)2016 Broadcom. All rights reserved.
  *
- *  THIS SOFTWARE MAY ONLY BE USED SUBJECT TO AN EXECUTED SOFTWARE LICENSE
- *  AGREEMENT  BETWEEN THE USER AND BROADCOM.  YOU HAVE NO RIGHT TO USE OR
- *  EXPLOIT THIS MATERIAL EXCEPT SUBJECT TO THE TERMS OF SUCH AN AGREEMENT.
+ * This program is the proprietary software of Broadcom and/or its licensors,
+ * and may only be used, duplicated, modified or distributed pursuant to the terms and
+ * conditions of a separate, written license agreement executed between you and Broadcom
+ * (an "Authorized License").  Except as set forth in an Authorized License, Broadcom grants
+ * no license (express or implied), right to use, or waiver of any kind with respect to the
+ * Software, and Broadcom expressly reserves all rights in and to the Software and all
+ * intellectual property rights therein.  IF YOU HAVE NO AUTHORIZED LICENSE, THEN YOU
+ * HAVE NO RIGHT TO USE THIS SOFTWARE IN ANY WAY, AND SHOULD IMMEDIATELY
+ * NOTIFY BROADCOM AND DISCONTINUE ALL USE OF THE SOFTWARE.
  *
- * $brcm_Workfile: $
- * $brcm_Revision: $
- * $brcm_Date: $
+ * Except as expressly set forth in the Authorized License,
+ *
+ * 1.     This program, including its structure, sequence and organization, constitutes the valuable trade
+ * secrets of Broadcom, and you shall use all reasonable efforts to protect the confidentiality thereof,
+ * and to use this information only in connection with your use of Broadcom integrated circuit products.
+ *
+ * 2.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, THE SOFTWARE IS PROVIDED "AS IS"
+ * AND WITH ALL FAULTS AND BROADCOM MAKES NO PROMISES, REPRESENTATIONS OR
+ * WARRANTIES, EITHER EXPRESS, IMPLIED, STATUTORY, OR OTHERWISE, WITH RESPECT TO
+ * THE SOFTWARE.  BROADCOM SPECIFICALLY DISCLAIMS ANY AND ALL IMPLIED WARRANTIES
+ * OF TITLE, MERCHANTABILITY, NONINFRINGEMENT, FITNESS FOR A PARTICULAR PURPOSE,
+ * LACK OF VIRUSES, ACCURACY OR COMPLETENESS, QUIET ENJOYMENT, QUIET POSSESSION
+ * OR CORRESPONDENCE TO DESCRIPTION. YOU ASSUME THE ENTIRE RISK ARISING OUT OF
+ * USE OR PERFORMANCE OF THE SOFTWARE.
+ *
+ * 3.     TO THE MAXIMUM EXTENT PERMITTED BY LAW, IN NO EVENT SHALL BROADCOM OR ITS
+ * LICENSORS BE LIABLE FOR (i) CONSEQUENTIAL, INCIDENTAL, SPECIAL, INDIRECT, OR
+ * EXEMPLARY DAMAGES WHATSOEVER ARISING OUT OF OR IN ANY WAY RELATING TO YOUR
+ * USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF BROADCOM HAS BEEN ADVISED OF
+ * THE POSSIBILITY OF SUCH DAMAGES; OR (ii) ANY AMOUNT IN EXCESS OF THE AMOUNT
+ * ACTUALLY PAID FOR THE SOFTWARE ITSELF OR U.S. $1, WHICHEVER IS GREATER. THESE
+ * LIMITATIONS SHALL APPLY NOTWITHSTANDING ANY FAILURE OF ESSENTIAL PURPOSE OF
+ * ANY LIMITED REMEDY.
  *
  * [File Description:]
  *
- * Revision History:
- *
- * $brcm_Log: $
- * 
  ***************************************************************************/
 
 /* base modules */
@@ -298,7 +318,7 @@ BVCE_Platform_P_GetConfig(
       pstPlatformConfig->stMailbox.uiHost2ViceInterruptAddress = BCHP_VICE2_ARCSS_ESS_P1_INTR2_0_0_ARC_P0_SET + iInstanceRegisterOffset;
       pstPlatformConfig->stMailbox.uiHost2ViceInterruptMask = BCHP_VICE2_ARCSS_ESS_P1_INTR2_0_0_ARC_P0_SET_ARCESS_SOFT1_10_INTR_MASK;
 
-   #ifdef BCHP_PWR_SUPPORT
+#ifdef BCHP_PWR_SUPPORT
       switch ( uiInstance )
       {
          case 0:
@@ -325,6 +345,30 @@ BVCE_Platform_P_GetConfig(
             return BERR_TRACE( BERR_NOT_SUPPORTED );
       }
 #endif
+
+      pstPlatformConfig->stPower.stCore[0].stSleep.uiAddress = BCHP_VICE2_ARCSS_ESS_HOSTIF_0_0_HOSTIFi_ARRAY_BASE + 0x14 + iInstanceRegisterOffset;
+      pstPlatformConfig->stPower.stCore[0].stSleep.uiMask = (((uint32_t) 1) << 23);
+      pstPlatformConfig->stPower.stCore[0].stSleep.uiValue = 0xFFFFFFFF;
+
+      pstPlatformConfig->stPower.stCore[0].stWatchdog.uiAddress = BCHP_VICE2_ARCSS_ESS_CTRL_0_0_INIT_SYS_WATCHDOG_CTRL + iInstanceRegisterOffset;
+      pstPlatformConfig->stPower.stCore[0].stWatchdog.uiMask = BCHP_VICE2_ARCSS_ESS_CTRL_0_0_INIT_SYS_WATCHDOG_CTRL_ENABLE_MASK;
+      pstPlatformConfig->stPower.stCore[0].stWatchdog.uiValue = 0xFFFFFFFF;
+
+      pstPlatformConfig->stPower.stCore[0].stHalt.uiAddress = BCHP_VICE2_ARCSS_ESS_HOSTIF_0_0_HOSTIFi_ARRAY_BASE + 0x28 + iInstanceRegisterOffset;
+      pstPlatformConfig->stPower.stCore[0].stHalt.uiMask = 0x01;
+      pstPlatformConfig->stPower.stCore[0].stHalt.uiValue = 0xFFFFFFFF;
+
+      pstPlatformConfig->stPower.stCore[1].stSleep.uiAddress = BCHP_VICE2_ARCSS_ESS_HOSTIF_1_0_HOSTIFi_ARRAY_BASE + 0x14 + iInstanceRegisterOffset;
+      pstPlatformConfig->stPower.stCore[1].stSleep.uiMask = (((uint32_t) 1) << 23);
+      pstPlatformConfig->stPower.stCore[1].stSleep.uiValue = 0xFFFFFFFF;
+
+      pstPlatformConfig->stPower.stCore[1].stWatchdog.uiAddress = BCHP_VICE2_ARCSS_ESS_CTRL_1_0_INIT_SYS_WATCHDOG_CTRL + iInstanceRegisterOffset;
+      pstPlatformConfig->stPower.stCore[1].stWatchdog.uiMask = BCHP_VICE2_ARCSS_ESS_CTRL_1_0_INIT_SYS_WATCHDOG_CTRL_ENABLE_MASK;
+      pstPlatformConfig->stPower.stCore[1].stWatchdog.uiValue = 0xFFFFFFFF;
+
+      pstPlatformConfig->stPower.stCore[1].stHalt.uiAddress = BCHP_VICE2_ARCSS_ESS_HOSTIF_1_0_HOSTIFi_ARRAY_BASE + 0x28 + iInstanceRegisterOffset;
+      pstPlatformConfig->stPower.stCore[1].stHalt.uiMask = 0x01;
+      pstPlatformConfig->stPower.stCore[1].stHalt.uiValue = 0xFFFFFFFF;
 
       pstPlatformConfig->stWatchdogRegisterDumpList.iInstanceOffset = BVCE_P_REGISTER_BASE + iInstanceRegisterOffset;
       pstPlatformConfig->stWatchdogRegisterDumpList.astRegisters = s_astViceHardwareRegisters;
