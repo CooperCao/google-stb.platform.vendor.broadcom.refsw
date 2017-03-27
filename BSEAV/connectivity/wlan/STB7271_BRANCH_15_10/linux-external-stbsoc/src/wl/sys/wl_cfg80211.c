2fb6c6ed406a36d3a5c42c06195a177aadac846f
YT Park <yt.park@broadcom.com>
2017-03-27 11:29:37 -0700

    SWANDROID-3881: RB:759 Fix to set wpa_supplicant scan timestamp from boottime
    Change-Id: If06b8adc439d7eec34ac1a86ea0051f4d2965ced

--- BSEAV/connectivity/wlan/STB7271_BRANCH_15_10/linux-external-stbsoc/src/wl/sys/wl_cfg80211.c
+++ BSEAV/connectivity/wlan/STB7271_BRANCH_15_10/linux-external-stbsoc/src/wl/sys/wl_cfg80211.c
@@ -11185,8 +11185,8 @@ static s32 wl_inform_single_bss(struct bcm_cfg80211 *cfg, struct wl_bss_info *bi
 				+ ts.tv_nsec / 1000;
 #else
 		struct timeval tv;
-		do_gettimeofday(&tv);
-		mgmt->u.probe_resp.timestamp = ((u64)tv.tv_sec*1000000)
+		jiffies_to_timeval(jiffies, &tv);
+		mgmt->u.probe_resp.timestamp = (((u64)tv.tv_sec + 300)*1000000)
 				+ tv.tv_usec;
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(2, 6, 39) &&
 	* (BCMDONGLEHOST || PLATFORM_INTEGRATED_WIFI)
