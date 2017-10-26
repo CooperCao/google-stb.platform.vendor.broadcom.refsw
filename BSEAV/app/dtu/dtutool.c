285ec97b308dfe72ffaab34b953fb04db8241809
Jerry Wong <jerry.wong@broadcom.com>
2017-10-25 20:57:39 -0700

    Revert "Revert "SWSTB-6795: Restore initial definition of "end of list" for DTU remap""
    This reverts commit fc274f3b6a0adcc458ce28fbe62d0af5adae9078.

--- BSEAV/app/dtu/dtutool.c
+++ BSEAV/app/dtu/dtutool.c
@@ -50,10 +50,14 @@
 #include "bchp.h"
 #include "bchp_common.h"
 #include "bdtu.h"
+#include "bchp_memc_clients.h"
+#include "../src/bdtu_map.inc" /* TEMP for BDTU_P_ReadMappingInfo */
 
 BDBG_MODULE(dtutool);
 
 static int g_fd;
+
+#define _2MB (2*1024*1024)
 
 enum ownerID {
     dtu_owner_eBsp_0 = 0,
@@ -126,6 +130,7 @@ static void print_usage(void)
     "-restore         restore identity map and reset cma\n"
     "-scrub ADDR,SIZE\n"
     "-show_owners     show page ownership\n"
+    "-dump ADDR,SIZE\n"
     );
 }
 
@@ -134,6 +139,35 @@ enum maptype {
     maptype_identity,
     maptype_remapped
 };
+
+static uint8_t addr_to_memc(BSTD_DeviceOffset addr, BDTU_CreateSettings settings[BCHP_P_MEMC_COUNT])
+{
+    uint8_t memc, region;
+    BSTD_DeviceOffset end;
+
+    for (memc=0; memc<BCHP_P_MEMC_COUNT; memc++) {
+        for (region=0; region<BCHP_MAX_MEMC_REGIONS; region++) {
+            if(!settings[memc].memoryLayout.region[region].size) {
+                continue;
+            }
+            end = settings[memc].memoryLayout.region[region].addr + settings[memc].memoryLayout.region[region].size;
+            end -= _2MB;
+
+            if((addr >= settings[memc].memoryLayout.region[region].addr) && (addr <= end)) {
+                goto FOUND;
+            }
+        }
+    }
+
+FOUND:
+
+    if(memc>=BCHP_P_MEMC_COUNT) {
+        BDBG_ERR(("Cannot determine memc. Defaulting to 0. May fail"));
+        memc = 0;
+    }
+
+    return memc;
+}
 
 static void print_map(enum maptype maptype, uint64_t fromaddr, uint64_t toaddr)
 {
@@ -145,21 +179,20 @@ int main(int argc, char **argv)
 {
     BREG_Handle reg;
     int rc;
-    BDTU_Handle dtu;
-    BDTU_CreateSettings settings;
+    BDTU_Handle dtu[BCHP_P_MEMC_COUNT];
+    BDTU_CreateSettings settings[BCHP_P_MEMC_COUNT];
     int curarg = 1;
     enum {
         action_help,
         action_print,
         action_restore,
         action_scrub,
-        action_show_owners
+        action_show_owners,
+        action_dump
     } action = action_help;
-#define _2MB (2*1024*1024)
-    uint64_t dtu_base_unmapped_ba = (uint64_t)2*1024*1024*1024; /* TODO: chip specific */
-    uint64_t dtu_max_ba = (uint64_t)4*1024*1024*1024; /* TODO: chip specific */
-    unsigned scrub_addr = 0, scrub_size;
-    BDTU_Status status;
+    BSTD_DeviceOffset scrub_addr = 0, scrub_size;
+    BDTU_Status status[BCHP_P_MEMC_COUNT];
+    unsigned i;
 
     while (curarg < argc) {
         if (!strcmp(argv[curarg], "-h") || !strcmp(argv[curarg], "--help")) {
@@ -176,14 +209,27 @@ int main(int argc, char **argv)
             char *s = strchr(argv[++curarg], ',');
             if (s) {
                 *s++ = 0;
-                scrub_addr = strtoul(argv[curarg], NULL, 0);
-                scrub_size = strtoul(s, NULL, 0);
+                scrub_addr = strtoull(argv[curarg], NULL, 0);
+                scrub_size = strtoull(s, NULL, 0);
             }
             if (!scrub_addr || !scrub_size) {
                 print_usage();
                 return -1;
             }
             action = action_scrub;
+        }
+        else if (!strcmp(argv[curarg], "-dump") && curarg+1<argc) {
+            char *s = strchr(argv[++curarg], ',');
+            if (s) {
+                *s++ = 0;
+                scrub_addr = strtoull(argv[curarg], NULL, 0);
+                scrub_size = strtoull(s, NULL, 0);
+            }
+            if (!scrub_size) {
+                print_usage();
+                return -1;
+            }
+            action = action_dump;
         }
         else if (!strcmp(argv[curarg], "-show_owners")) {
             action = action_show_owners;
@@ -204,59 +250,88 @@ int main(int argc, char **argv)
     BDBG_Init();
     reg = map_registers();
 
-    BDTU_GetDefaultCreateSettings(&settings);
-    settings.reg = reg;
-    settings.memcIndex = 0;
-    settings.physAddrBase = 0;
-    rc = BDTU_Create(&dtu, &settings);
-    if (rc) {BERR_TRACE(rc); goto done;}
+    for (i=0; i<BCHP_P_MEMC_COUNT; i++) {
+        BDTU_GetDefaultCreateSettings(&settings[i]);
+        settings[i].reg = reg;
+        BDTU_P_ReadMappingInfo(reg, i, &settings[i].memoryLayout);
+        rc = BDTU_Create(&dtu[i], &settings[i]);
+        if (rc) {BERR_TRACE(rc); goto done;}
 
-    rc = BDTU_GetStatus(dtu, &status);
-    if (rc) {BERR_TRACE(rc); goto done;}
+        rc = BDTU_GetStatus(dtu[i], &status[i]);
+        if (rc) {BERR_TRACE(rc); goto done;}
 
-    if (status.state != BDTU_State_eEnabled) {
-        BDBG_ERR(("DTU not enabled: state %u", status.state));
+        if (status[i].state != BDTU_State_eEnabled) {
+            BDBG_ERR(("DTU[%d] not enabled: state %u", i, status[i].state));
+        }
     }
 
     if (action == action_print) {
-        uint64_t addr = 0, prev_addr = 0, devaddr;
-        enum maptype prev_maptype = maptype_identity, maptype;
-        for (;addr<dtu_max_ba;addr+=_2MB) {
-            rc = BDTU_ReadDeviceAddress(dtu, addr, &devaddr);
-            if (rc) {
-                maptype = maptype_unmapped;
-            }
-            else {
-                maptype = addr==devaddr?maptype_identity:maptype_remapped;
-            }
-            if (maptype != prev_maptype) {
-                print_map(prev_maptype, prev_addr, addr);
-                prev_maptype = maptype;
-                prev_addr = addr;
+        uint64_t addr = 0, prev_addr = 0, org_addr;
+        uint8_t memc;
+        BDTU_MappingInfo mapping;
+
+        for (memc=0; memc<BCHP_P_MEMC_COUNT; memc++) {
+            mapping = settings[memc].memoryLayout;
+            for (i=0; i<BCHP_MAX_MEMC_REGIONS; i++) {
+                enum maptype prev_maptype = maptype_identity, maptype;
+                addr = mapping.region[i].addr;
+                prev_addr = mapping.region[i].addr;
+                if (!mapping.region[i].size) {
+                    continue;
+                }
+                for (;mapping.region[i].size;mapping.region[i].size-=_2MB) {
+                    rc = BDTU_ReadOriginalAddress(dtu[memc], addr, &org_addr);
+
+                    if (rc) {
+                        maptype = maptype_unmapped;
+                    }
+                    else {
+                        maptype = addr==org_addr?maptype_identity:maptype_remapped;
+                    }
+                    if (maptype != prev_maptype) {
+                        if (prev_addr != addr) {
+                            print_map(prev_maptype, prev_addr, addr);
+                        }
+                        prev_maptype = maptype;
+                        prev_addr = addr;
+                    }
+                    addr += _2MB;
+                }
+                if (prev_addr != addr) {
+                    print_map(prev_maptype, prev_addr, addr);
+                }
             }
         }
-        print_map(prev_maptype, prev_addr, addr);
     }
     else if (action == action_restore) {
         /* first, remap back to identity. */
-        uint64_t addr = 0, devaddr;
+        uint64_t addr = 0, org_addr;
         unsigned n = 0;
         unsigned failures = 0;
+        uint8_t memc;
+        BSTD_DeviceOffset end;
+
         rc = 0;
-        for (;addr<dtu_max_ba;addr+=_2MB) {
-            rc = BDTU_ReadDeviceAddress(dtu, addr, &devaddr);
-            if (!rc && addr != devaddr) {
-                BDTU_RemapSettings settings;
-                n++;
-                BDTU_GetDefaultRemapSettings(&settings);
-                settings.list[0].devAddr = devaddr;
-                settings.list[0].fromPhysAddr = addr;
-                settings.list[0].toPhysAddr = devaddr;
-                rc = BDTU_Remap(dtu, &settings);
-                if (rc) { BDBG_ERR(("unable to restore ba " BDBG_UINT64_FMT, addr)); failures++;}
+        for (memc=0; memc<BCHP_P_MEMC_COUNT; memc++) {
+            for (i=0; i<BCHP_MAX_MEMC_REGIONS; i++) {
+                addr = settings[memc].memoryLayout.region[i].addr;
+                end = addr + settings[memc].memoryLayout.region[i].size;
+
+                for (;addr<end;addr+=_2MB) {
+                    rc = BDTU_ReadOriginalAddress(dtu[memc], addr, &org_addr);
+                    if (!rc && addr != org_addr) {
+                        BDTU_RemapSettings settings;
+                        n++;
+                        BDTU_GetDefaultRemapSettings(&settings);
+                        settings.list[0].orgPhysAddr = org_addr;
+                        settings.list[0].fromPhysAddr = addr;
+                        settings.list[0].toPhysAddr = org_addr;
+                        rc = BDTU_Remap(dtu[memc], &settings);
+                        if (rc) { BDBG_ERR(("unable to restore ba " BDBG_UINT64_FMT, addr)); failures++;}
+                    }
+                }
             }
         }
-
         /* second, if no remap failures, reset cma. */
         if (!failures) {
             system("cmatool resetall");
@@ -264,7 +339,8 @@ int main(int argc, char **argv)
         }
     }
     else if (action == action_scrub) {
-        rc = BDTU_Scrub(dtu, scrub_addr, scrub_size);
+        /* No checks on size here, BDTU call should fail in invalid */
+        rc = BDTU_Scrub(dtu[addr_to_memc(scrub_addr, settings)], scrub_addr, scrub_size);
         if (rc) BERR_TRACE(rc);
     }
     else if (action == action_show_owners) {
@@ -274,43 +350,73 @@ int main(int argc, char **argv)
         enum ownedState {eOS_unknown, eOS_owned, eOS_notOwned} owned=eOS_unknown, lastOwned=eOS_unknown;
         enum validState {eVS_unknown, eVS_valid, eVS_invalid} valid=eVS_unknown, lastValid=eVS_unknown;
         uint8_t lastOwner=~0, owner=~0;
+        uint8_t memc;
+        BDTU_MappingInfo mapping;
 
-        for (;addr<dtu_max_ba;addr+=_2MB) {
-            rc = BDTU_ReadInfo(dtu, addr, &info);
-            if(rc!=BERR_SUCCESS)
-            {
-                BDBG_ERR(("Unable to obtain info on address " BDBG_UINT64_FMT, BDBG_UINT64_ARG(addr)));
-                goto done;
-            }
-
-            valid = info.valid ? eVS_valid : eVS_invalid;
-            if(valid==eVS_valid) {
-                owned = info.owned ? eOS_owned : eOS_notOwned;
-                owner = info.owned ? info.ownerID : ~0;
-            } else {
-                owned = eOS_unknown;
-            }
-
-            if((addr==0) || (valid != lastValid) || ((valid == eVS_valid) && (owned != lastOwned)) || (owner != lastOwner)) {
-                if(addr && (start!=(BSTD_DeviceOffset)~0))
-                {
-                    printf(" - " BDBG_UINT64_FMT "\n", BDBG_UINT64_ARG(addr-1));
+        for (memc=0; memc<BCHP_P_MEMC_COUNT; memc++) {
+            mapping = settings[memc].memoryLayout;
+            for (i=0; i<BCHP_MAX_MEMC_REGIONS; i++) {
+                addr = mapping.region[i].addr;
+                start = ~0;
+                owned = lastOwned = eOS_unknown;
+                valid = lastValid = eVS_unknown;
+                lastOwner = owner = ~0;
+                if (!mapping.region[i].size) {
+                    continue;
                 }
+                for (;mapping.region[i].size;mapping.region[i].size-=_2MB) {
+                    rc = BDTU_ReadInfo(dtu[memc], addr, &info);
+                    if(rc!=BERR_SUCCESS)
+                    {
+                        BDBG_ERR(("Unable to obtain info on address " BDBG_UINT64_FMT, BDBG_UINT64_ARG(addr)));
+                        goto done;
+                    }
 
-                printf("%s : %9.9s " BDBG_UINT64_FMT, (valid == eVS_valid) ? "VALID  " : "INVALID", (owned == eOS_owned) ? ownerID_to_name(owner) : "UNOWNED", BDBG_UINT64_ARG(addr));
-                start=addr;
+                    valid = info.valid ? eVS_valid : eVS_invalid;
+                    if(valid==eVS_valid) {
+                        owned = info.owned ? eOS_owned : eOS_notOwned;
+                        owner = info.owned ? info.ownerID : ~0;
+                    } else {
+                        owned = eOS_unknown;
+                    }
+
+                    if((addr==mapping.region[i].addr) || (valid != lastValid) || ((valid == eVS_valid) && (owned != lastOwned)) || (owner != lastOwner)) {
+                        if((addr!=mapping.region[i].addr) && (start!=(BSTD_DeviceOffset)~0))
+                        {
+                            printf(" - " BDBG_UINT64_FMT "\n", BDBG_UINT64_ARG(addr-1));
+                        }
+
+                        printf("MEMC[%d][%d] %s : %9.9s " BDBG_UINT64_FMT, mapping.memcIndex, i,
+                               (valid == eVS_valid) ? "VALID  " : "INVALID", (owned == eOS_owned) ? ownerID_to_name(owner) : "UNOWNED", BDBG_UINT64_ARG(addr));
+                        start=addr;
+                    }
+
+                    lastValid=valid;
+                    lastOwned=owned;
+                    lastOwner=owner;
+                    addr += _2MB;
+                }
+                printf(" - " BDBG_UINT64_FMT "\n", BDBG_UINT64_ARG(addr-1));
             }
-
-            lastValid=valid;
-            lastOwned=owned;
-            lastOwner=owner;
         }
-        printf(" - " BDBG_UINT64_FMT "\n", BDBG_UINT64_ARG(addr-1));
+    }
+    else if(action==action_dump)
+    {
+        BDBG_Level level = BDBG_eErr;
+        rc = BDBG_GetModuleLevel("BDTU", &level);
+        if (rc) BERR_TRACE(rc);
+        rc = BDBG_SetModuleLevel("BDTU", BDBG_eMsg);
+        if (rc) BERR_TRACE(rc);
+        BDTU_PrintMap(dtu[addr_to_memc(scrub_addr, settings)], scrub_addr, scrub_size);
+        rc = BDBG_SetModuleLevel("BDTU", level);
+        if (rc) BERR_TRACE(rc);
     }
 
 done:
-    if (dtu) {
-        BDTU_Destroy(dtu);
+    for (i=0; i<BCHP_P_MEMC_COUNT; i++) {
+        if (dtu[i]) {
+            BDTU_Destroy(dtu[i]);
+        }
     }
     BREG_Close(reg);
     BDBG_Uninit();
--- nexus/modules/core/src/nexus_memory.c
+++ nexus/modules/core/src/nexus_memory.c
@@ -43,6 +43,7 @@
 #include "b_objdb.h"
 #include "priv/nexus_base_platform.h"
 #include "bmma_range.h"
+#include "../src/bdtu_map.inc"
 
 BDBG_MODULE(nexus_memory);
 BDBG_FILE_MODULE(nexus_core);
@@ -210,7 +211,8 @@ static BERR_Code nexus_memory_p_dtu_alloc(NEXUS_HeapHandle heap, BMMA_DeviceOffs
     BDTU_RemapSettings *remapSettings;
     unsigned total = 0;
     size_t last_remapped_addr = 0;
-    BDBG_WRN(("dtu_alloc BA " BDBG_UINT64_FMT ", size %u MB from %s:%u", BDBG_UINT64_ARG(base), (unsigned)(size/1024/1024), fname, line));
+    BDBG_WRN(("dtu_alloc BA " BDBG_UINT64_FMT " (MEMC%u), size %u MB from %s:%u", BDBG_UINT64_ARG(base), heap->settings.memcIndex, (unsigned)(size/1024/1024), fname, line));
+
     if (base & (_2MB-1)) {
         return BERR_TRACE(BERR_INVALID_PARAMETER);
     }
@@ -230,14 +232,14 @@ static BERR_Code nexus_memory_p_dtu_alloc(NEXUS_HeapHandle heap, BMMA_DeviceOffs
             goto error;
         }
 
-        remapSettings->list[total].devAddr = deviceAddr;
+        remapSettings->list[total].orgPhysAddr = deviceAddr;
         remapSettings->list[total].fromPhysAddr = deviceAddr;
         remapSettings->list[total].toPhysAddr = base+i;
         total++;
 
         if (total == BDTU_REMAP_LIST_TOTAL || i+_2MB>=size) {
             if (total < BDTU_REMAP_LIST_TOTAL) {
-                remapSettings->list[total].devAddr = 0; /* terminate list */
+                remapSettings->list[total].orgPhysAddr = BDTU_INVALID_ADDR; /* terminate list */
             }
             if (secure) {
                 rc = g_NexusCore.cfg.cma.secure_remap(heap->settings.memcIndex, remapSettings);
@@ -258,7 +260,7 @@ static BERR_Code nexus_memory_p_dtu_alloc(NEXUS_HeapHandle heap, BMMA_DeviceOffs
 error:
     /* free whatever was not remapped */
     for (i=0;i<total;i++) {
-        g_NexusCore.cfg.cma.free(heap->settings.memcIndex, remapSettings->list[i].devAddr , _2MB);
+        g_NexusCore.cfg.cma.free(heap->settings.memcIndex, remapSettings->list[i].orgPhysAddr , _2MB);
     }
     /* then use nexus_memory_p_dtu_free to remap and free the remainder */
     if (last_remapped_addr) {
@@ -291,20 +293,20 @@ static void nexus_memory_p_dtu_free(NEXUS_HeapHandle heap, BMMA_DeviceOffset bas
     for (i=0;i<size;i+=_2MB) {
         uint64_t deviceAddr;
         BERR_Code rc;
-        rc = BDTU_ReadDeviceAddress(g_NexusCore.publicHandles.memc[heap->settings.memcIndex].dtu, base + i, &deviceAddr);
+        rc = BDTU_ReadOriginalAddress(g_NexusCore.publicHandles.memc[heap->settings.memcIndex].dtu, base + i, &deviceAddr);
         if (rc) {
             BERR_TRACE(rc);
             continue;
         }
 
-        remapSettings->list[total].devAddr = deviceAddr;
+        remapSettings->list[total].orgPhysAddr = deviceAddr;
         remapSettings->list[total].fromPhysAddr = base+i;
         remapSettings->list[total].toPhysAddr = deviceAddr;
         total++;
 
         if (total == BDTU_REMAP_LIST_TOTAL || i+_2MB>=size) {
             if (total < BDTU_REMAP_LIST_TOTAL) {
-                remapSettings->list[total].devAddr = 0; /* terminate list */
+                remapSettings->list[total].orgPhysAddr = BDTU_INVALID_ADDR; /* terminate list */
             }
             /* map back to identity */
             if (secure) {
@@ -319,7 +321,7 @@ static void nexus_memory_p_dtu_free(NEXUS_HeapHandle heap, BMMA_DeviceOffset bas
             else {
                 unsigned j;
                 for (j=0;j<total;j++) {
-                    g_NexusCore.cfg.cma.free(heap->settings.memcIndex, remapSettings->list[j].devAddr, _2MB);
+                    g_NexusCore.cfg.cma.free(heap->settings.memcIndex, remapSettings->list[j].orgPhysAddr, _2MB);
                 }
             }
             total = 0;
@@ -505,8 +507,7 @@ NEXUS_HeapHandle NEXUS_Heap_Create_priv( unsigned index, BREG_Handle reg, const 
             BDTU_CreateSettings createSettings;
             BDTU_GetDefaultCreateSettings(&createSettings);
             createSettings.reg = reg;
-            createSettings.memcIndex = heap->settings.memcIndex;
-            createSettings.physAddrBase = g_NexusCore.publicHandles.memoryLayout.memc[heap->settings.memcIndex].region[0].addr;
+            BDTU_P_ReadMappingInfo(reg, heap->settings.memcIndex, &createSettings.memoryLayout);
             rc = BDTU_Create(&g_NexusCore.publicHandles.memc[heap->settings.memcIndex].dtu, &createSettings);
             if (rc!=BERR_SUCCESS) { rc=BERR_TRACE(rc); goto error; }
         }
